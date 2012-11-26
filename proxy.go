367b123a2645f57abba4fcd0ae805453ab77b2c8
James Henstridge <james@jamesh.id.au>
2012-11-26 16:50:23 +0800

    In-progress sketch of what object proxies could look like.  Eventually we should support something like:
        proxy := conn.GetObject(busName, path)
        propsIface := &Properties{proxy}
        value, err := propsIface.Get(iface, name)
    
    With construction of the message, and marshalling the reply or error 
    back to Go types happening behind the scenes.

---
+++ proxy.go
@@ -0,0 +1,197 @@
+package dbus
+
+// This is not yet finished: it is an idea for what statically generated object bindings could look like.
+
+type ObjectProxy struct {
+	conn *Connection
+	destination string
+	path ObjectPath
+}
+
+func (o *ObjectProxy) GetObjectPath() ObjectPath {
+	return o.path
+}
+
+func (o *ObjectProxy) Call(iface, method string, args ...interface{}) (*Message, error) {
+	msg := NewMethodCallMessage(o.destination, o.path, iface, method)
+	if err := msg.AppendArgs(args...); err != nil {
+		return nil, err
+	}
+	reply, err := o.conn.SendWithReply(msg)
+	if err != nil {
+		return nil, err
+	}
+	if reply.Type == TypeError {
+		return nil, reply.AsError()
+	}
+	return reply, nil
+}
+
+type Introspectable struct {
+	*ObjectProxy
+}
+
+func (o *Introspectable) Introspect() (data string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus.Introspectable", "Introspect")
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&data)
+	return
+}
+
+type Properties struct {
+	*ObjectProxy
+}
+
+func (o *Properties) Get(interfaceName string, propertyName string) (value interface{}, err error) {
+	reply, err := o.Call("org.freedesktop.DBus.Properties", "Get", interfaceName, propertyName)
+	if err != nil {
+		return
+	}
+	var variant Variant
+	err = reply.GetArgs(&variant)
+	value = variant.Value
+	return
+}
+
+func (o *Properties) Set(interfaceName string, propertyName string, value interface{}) (err error) {
+	_, err = o.Call("org.freedesktop.DBus.Properties", "Set", interfaceName, propertyName, Variant{value})
+	return
+}
+
+func (o *Properties) GetAll(interfaceName string) (props map[string]Variant, err error) {
+	reply, err := o.Call("org.freedesktop.DBus.Properties", "GetAll", interfaceName)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&props)
+	return
+}
+
+type MessageBus struct {
+	*ObjectProxy
+}
+
+func (o *MessageBus) Hello() (uniqueName string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "Hello")
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&uniqueName)
+	return
+}
+
+func (o *MessageBus) RequestName(name string, flags uint32) (result uint32, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "RequestName", name, flags)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&result)
+	return
+}
+
+func (o *MessageBus) ReleaseName(name string) (result uint32, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "ReleaseName", name)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&result)
+	return
+}
+
+func (o *MessageBus) ListQueuedOwners(name string) (owners []string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "ListQueuedOwners", name)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&owners)
+	return
+}
+
+func (o *MessageBus) ListNames() (names []string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "ListNames")
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&names)
+	return
+}
+
+func (o *MessageBus) ListActivatableNames() (names []string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "ListActivatableNames")
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&names)
+	return
+}
+
+func (o *MessageBus) NameHasOwner(name string) (hasOwner bool, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "NameHasOwner", name)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&hasOwner)
+	return
+}
+
+func (o *MessageBus) StartServiceByName(name string, flags uint32) (result uint32, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "StartServiceByName", name, flags)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&result)
+	return
+}
+
+func (o *MessageBus) UpdateActivationEnvironment(env map[string]string) (err error) {
+	_, err = o.Call("org.freedesktop.DBus", "UpdateActivationEnvironment", env)
+	return
+}
+
+func (o *MessageBus) GetNameOwner(name string) (owner string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "GetNameOwner", name)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&owner)
+	return
+}
+
+func (o *MessageBus) GetConnectionUnixUser(busName string) (user uint32, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "GetConnectionUnixUser", busName)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&user)
+	return
+}
+
+func (o *MessageBus) GetConnectionUnixProcessID(busName string) (process uint32, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "GetConnectionUnixProcessID", busName)
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&process)
+	return
+}
+
+func (o *MessageBus) AddMatch(rule string) (err error) {
+	_, err = o.Call("org.freedesktop.DBus", "AddMatch", rule)
+	return
+}
+
+func (o *MessageBus) RemoveMatch(rule string) (err error) {
+	_, err = o.Call("org.freedesktop.DBus", "RemoveMatch", rule)
+	return
+}
+
+func (o *MessageBus) GetId() (busId string, err error) {
+	reply, err := o.Call("org.freedesktop.DBus", "GetId")
+	if err != nil {
+		return
+	}
+	err = reply.GetArgs(&busId)
+	return
+}
